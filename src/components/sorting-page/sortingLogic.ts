41b0b287c9f9b0d7ef77e9bcaf41e2e4cc6c3616
Alexandr Gerasimov <phantom6391@yandex.ru>
2023-05-01 15:13:21 +0400

    Стек

--- src/components/sorting-page/sortingLogic.ts
+++ src/components/sorting-page/sortingLogic.ts
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction } from "react";
 import { ElementStates } from "../../types/element-states";
 import { delay } from "../../utils/utils";
 import { SHORT_DELAY_IN_MS } from "../../constants/delays";
